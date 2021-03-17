714767d27c019045429d4b912ec1d97fe8eedf03
oorrppp2 <oorrppp@kist.re.kr>
2021-03-17 20:34:36 +0900

    Find the closest target

--- convert_2d_to_3d/src/generate_msg.cpp
+++ convert_2d_to_3d/src/generate_msg.cpp
@@ -35,7 +35,8 @@ class ConvertBoundingBoxNode
         {
 			pointcloud_sub_ = nh_.subscribe<sensor_msgs::PointCloud2>("/camera/depth_registered/points", 1, &ConvertBoundingBoxNode::pointcloud_callback, this);
 			boundingboxes_sub_ = nh_.subscribe<darknet_ros_msgs::BoundingBoxes>("/darknet_ros/bounding_boxes", 1, &ConvertBoundingBoxNode::boundingbox_callback, this);
-			target_id_sub_ = nh_.subscribe<std_msgs::String>("/remove_points_request", 1, &ConvertBoundingBoxNode::target_id_callback, this);
+			remove_request_sub_ = nh_.subscribe<std_msgs::String>("/remove_points_request", 1, &ConvertBoundingBoxNode::remove_request_callback, this);
+			target_id_sub_ = nh_.subscribe<std_msgs::String>("/request_target", 1, &ConvertBoundingBoxNode::target_id_callback, this);
 			pause_sub_ = nh_.subscribe<std_msgs::String>("/pause_request", 1, &ConvertBoundingBoxNode::pause_sub_callback, this);
 			pub_result_ = nh_.advertise<convert_2d_to_3d::Result>("detected_object", 1);
 			pointcloud_pub = nh_.advertise<sensor_msgs::PointCloud2>("/camera/depth_registered/removed_object_points", 1);
@@ -62,6 +63,10 @@ class ConvertBoundingBoxNode
 		}
 	}
 
+	void remove_request_callback(const std_msgs::StringConstPtr& msg) {
+		remove_request = msg->data;
+	}
+
 	void target_id_callback(const std_msgs::StringConstPtr& msg) {
 		target_id = msg->data;
 	}
@@ -70,7 +75,6 @@ class ConvertBoundingBoxNode
 		if(msg->data == "pause") {
 			pause_state = true;
 		} else if(msg->data == "resume") {
-			target_id = msg->data;
 			pause_state = false;
 		}
 	}
@@ -91,51 +95,54 @@ class ConvertBoundingBoxNode
 			double y = 0.0;
 			double z = 0.0;
 			std::string id = "";
+			pcl::PointXYZRGB mClosestPoint;
+			bool isFistPoint = true;
+			// std::cout << "======================================" << std::endl;
 
 			for(int i = 0; i < gBoundingboxes.bounding_boxes.size(); i++) {
+				id = gBoundingboxes.bounding_boxes[i].Class;
+				if(id != target_id) {
+					continue;
+				}
 				pcl::PointXYZRGB p = pcl_cloud( (gBoundingboxes.bounding_boxes[i].xmin + gBoundingboxes.bounding_boxes[i].xmax)/2,
 												(gBoundingboxes.bounding_boxes[i].ymin + gBoundingboxes.bounding_boxes[i].ymax)/2);
 				x = p.x;
 				y = p.y;
 				z = p.z;
-				id = gBoundingboxes.bounding_boxes[i].Class;
-				
-				// std::cout <<"point : " << p << std::endl;
-				// std::cout <<" ==> " << p.x << " , " << p.y << " , " << p.z << std::endl;
-				// std::cout << "ID : " << id << std::endl;
-				// std::cout << "target_id : " << target_id << std::endl;
-
-				if(id == target_id) {
-					// std::cout << target_id << " points removed!\t";
-					// std::cout << "remove_padding : " << remove_padding << std::endl;
+				// std::cout << "id : " << id << " / " << p << std::endl;
+				if(isFistPoint) {
+					mClosestPoint = p;
 					last_boundingbox_xmin = gBoundingboxes.bounding_boxes[i].xmin - remove_padding;
 					last_boundingbox_xmax = gBoundingboxes.bounding_boxes[i].xmax + remove_padding;
 					last_boundingbox_ymin = gBoundingboxes.bounding_boxes[i].ymin - remove_padding;
 					last_boundingbox_ymax = gBoundingboxes.bounding_boxes[i].ymax + remove_padding;
+					isFistPoint = false;
+				} else if(z < mClosestPoint.z) {
+					mClosestPoint = p;
+					last_boundingbox_xmin = gBoundingboxes.bounding_boxes[i].xmin - remove_padding;
+					last_boundingbox_xmax = gBoundingboxes.bounding_boxes[i].xmax + remove_padding;
+					last_boundingbox_ymin = gBoundingboxes.bounding_boxes[i].ymin - remove_padding;
+					last_boundingbox_ymax = gBoundingboxes.bounding_boxes[i].ymax + remove_padding;
+				}
 
-					// it = pcl_cloud.begin();
-					// for(int i = 0; i < last_boundingbox_ymax - last_boundingbox_ymin; i++) {
-					// 	std::cout << "i : " << i << std::endl;
-					// 	std::cout << "width : " << pcl_cloud.width << std::endl;
-					// 	pcl_cloud.erase(it + y * pcl_cloud.width + last_boundingbox_xmin,
-					// 					it + y * pcl_cloud.width + last_boundingbox_xmax);
-					// }
+			}
 
+			if(!isFistPoint) {
+				x = mClosestPoint.x;
+				y = mClosestPoint.y;
+				z = mClosestPoint.z;
+				if(remove_request == "remove") {
 					for(int x = last_boundingbox_xmin; x < last_boundingbox_xmax; x++) {
 						for(int y = last_boundingbox_ymin; y < last_boundingbox_ymax; y++) {
 							pcl_cloud(x, y).x, pcl_cloud(x, y).y = INFINITY;
 							pcl_cloud(x, y).z = INFINITY;
-							// pcl_cloud(x,y).a = 0;
-							// pcl_cloud(x, y)->
 						}
 					}
-					// std::cout << "isDense : " << pcl_cloud.is_dense << std::endl;
 				}
-
 				// Publish result
 				convert_2d_to_3d::Result msg;
 				msg.type = "detected_object";
-				msg.data = id;
+				msg.data = target_id;
 
 				msg.pose.header.stamp = ros::Time::now();
 				msg.pose.header.frame_id = "object_coordinate";
@@ -152,21 +159,11 @@ class ConvertBoundingBoxNode
 			}
 
 			pointcloud_pub.publish(pcl_cloud);
-
 			paused_pcl_cloud = pcl_cloud;
 
 			ros::Duration(0.01).sleep();
 			gBoundingboxes.bounding_boxes.clear();
 		}
-		// else {
-		// 	for(int x = last_boundingbox_xmin - remove_padding; x < last_boundingbox_xmax + remove_padding; x++) {
-		// 		for(int y = last_boundingbox_ymin - remove_padding; y < last_boundingbox_ymax + remove_padding; y++) {
-		// 			// pcl_cloud(x, y).x, pcl_cloud(x, y).y = 0;
-		// 			pcl_cloud(x, y).y = 10;
-		// 		}
-		// 	}
-		// 	pointcloud_pub.publish(pcl_cloud);
-		// }
 	}
 
     private:
@@ -174,12 +171,14 @@ class ConvertBoundingBoxNode
         tf2_ros::TransformBroadcaster tfb_;
 	ros::Subscriber pointcloud_sub_;
 	ros::Subscriber boundingboxes_sub_;
+	ros::Subscriber remove_request_sub_;
 	ros::Subscriber target_id_sub_;
 	ros::Subscriber pause_sub_;
 	ros::Publisher pub_result_;
 	ros::Publisher pointcloud_pub;
 	darknet_ros_msgs::BoundingBoxes gBoundingboxes;
 	std::string target_id;
+	std::string remove_request;
 	pcl::PointCloud<pcl::PointXYZRGB> paused_pcl_cloud;
 
 	int last_boundingbox_xmin;
